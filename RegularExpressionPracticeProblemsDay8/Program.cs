177d1d776cab1acc4ad4b1d040980b07f7651287
Vivek Verma <vverma240420@gmail.com>
2023-07-14 19:53:30 +0530

    Added regex for fetching html tag

--- RegularExpressionPracticeProblemsDay8/Program.cs
+++ RegularExpressionPracticeProblemsDay8/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace RegularExpressionPracticeProblemsDay8
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionPracticeProblemsDay8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regular Expression Programs.");
-            Console.ReadLine();
+            string input = "<p>The <code>Regex</code> is a compiled representation of a regular expression.</p>";
+            string pattern = @"<[^>]+>";                                                  //<-matches the opening
+            Regex regex = new Regex(pattern);                                             // [^>] - matches one or more occurences
+            MatchCollection matches = regex.Matches(input);                               
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
         }
     }
 }
