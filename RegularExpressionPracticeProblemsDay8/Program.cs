b8d980e540fa0a90e4c21dc33bd2ab3c8da8288d
Vivek Verma <vverma240420@gmail.com>
2023-07-14 19:33:37 +0530

    Added the program {find sequence of lowercase letters

--- RegularExpressionPracticeProblemsDay8/Program.cs
+++ RegularExpressionPracticeProblemsDay8/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace RegularExpressionPracticeProblemsDay8
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionPracticeProblemsDay8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regular Expression Programs.");
-            Console.ReadLine();
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+   
+            string pattern = @"[a-z]+_[a-z]+";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("Sequences of lowercase letters joined by an underscore:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No sequences of lowercase letters joined by an underscore found.");
+            }
         }
     }
 }
