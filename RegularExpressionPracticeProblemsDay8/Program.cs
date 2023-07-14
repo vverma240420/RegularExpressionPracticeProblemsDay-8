6b59fb6fa3265abfda5e793514f06f6f8cf426f9
Vivek Verma <vverma240420@gmail.com>
2023-07-14 19:24:03 +0530

    Added regex program Answer-1

--- RegularExpressionPracticeProblemsDay8/Program.cs
+++ RegularExpressionPracticeProblemsDay8/Program.cs
@@ -1,11 +1,25 @@
-﻿namespace RegularExpressionPracticeProblemsDay8
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionPracticeProblemsDay8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regular Expression Programs.");
-            Console.ReadLine();
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            string pattern = @"ab{2,3}";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+
+            if (match.Success)
+            {
+                Console.WriteLine("Input string matches the pattern!");
+            }
+            else
+            {
+                Console.WriteLine("Input string does not match the pattern.");
+            }
         }
     }
 }
