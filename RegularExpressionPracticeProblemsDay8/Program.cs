0596bdf9caa6aedc5106d260016f1ac625bd5fb0
Vivek Verma <vverma240420@gmail.com>
2023-07-14 19:59:30 +0530

    Added the program {Count the number of occurences}

--- RegularExpressionPracticeProblemsDay8/Program.cs
+++ RegularExpressionPracticeProblemsDay8/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace RegularExpressionPracticeProblemsDay8
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionPracticeProblemsDay8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regular Expression Programs.");
-            Console.ReadLine();
+            string str1 = "foxes are omnivorous mammals belonging to several genera\r\nof the family Canidae fox.";
+            string str2 = "fox";
+            int freq = Regex.Matches(str1, str2.ToString()).Count();
+            Console.WriteLine(freq);
+
         }
     }
 }
